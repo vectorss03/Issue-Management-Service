4c45eb78f97c3a3a6f507efbdbdd66ab7a01c13f
Hysk <vectorss03@gmail.com>
2024-05-13 21:49:01 +0900

    Application project initial commit

---
+++ application/src/main/java/ApplicationExample.java
@@ -0,0 +1,10 @@
+public class ApplicationExample {
+    public static void main(String[] args) {
+        System.out.println(new ApplicationExample().helloWorld());
+        SwingView view = new SwingView();
+    }
+
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
---
+++ application/src/main/java/SwingView.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class SwingView extends JFrame {
+    public SwingView() {
+        setTitle("Test");
+        setSize(500, 500);
+        setVisible(true);
+    }
+}
