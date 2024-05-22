4159eee78774395150c05f79289bb849bf2a854a
Hyun Wook <hwkimcode@gmail.com>
2024-05-22 21:01:40 +0900

    FakeRepository added

---
+++ application/src/main/java/com/se14/repository/fake/CommentRepositoryFake.java
@@ -0,0 +1,30 @@
+package com.se14.repository.fake;
+
+import com.se14.domain.Comment;
+import com.se14.domain.Issue;
+import com.se14.repository.CommentRepository;
+
+import java.util.*;
+
+public class CommentRepositoryFake implements CommentRepository {
+
+    private final Map<Long, Comment> comments = new HashMap<>();
+    private long currentId = 1;
+
+    @Override
+    public Comment save(Comment comment, Issue issue) {
+        comments.put(currentId++, comment);
+        issue.getComments().add(comment);
+        return comment;
+    }
+
+    @Override
+    public Optional<Comment> findById(long id) {
+        return Optional.ofNullable(comments.get(id));
+    }
+
+    @Override
+    public List<Comment> findByIssue(Issue issue) {
+        return issue.getComments();
+    }
+}
