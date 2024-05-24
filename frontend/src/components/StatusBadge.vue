54977eee940c51f71bad46d355f822782a9f4185
hysk <vectorss03@gmail.com>
2024-05-24 18:03:56 +0900

    add Change State Modal

--- frontend/src/components/StatusBadge.vue
+++ frontend/src/components/StatusBadge.vue
@@ -2,7 +2,7 @@
   <span v-if="status === `NEW`" class="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full">New</span>
   <span v-if="status === `ASSIGNED`" class="bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full">Assigned</span>
   <span v-if="status === `FIXED`" class="bg-pink-100 text-pink-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full">Fixed</span>
-  <span v-if="status === `RESOLVED`" class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full0">Resolved</span>
+  <span v-if="status === `RESOLVED`" class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full">Resolved</span>
   <span v-if="status === `CLOSED`" class="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full">Closed</span>
   <span v-if="status === `REOPENED`" class="bg-cyan-100 text-cyan-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full">Reopened</span>
 </template>
--- frontend/src/views/Board/Issue/IssueDeatil.vue
+++ frontend/src/views/Board/Issue/IssueDeatil.vue
@@ -58,7 +58,8 @@
           <ol class="mx-16 items-center my-3 py-1">
             <li class="text-sm mb-3">
               <StatusBadge :status="issue.status.toUpperCase()"/>
-              <button class="mx-2.5 text-blue-700 text-xs px-4 hover:underline">Change State</button>
+              <button data-modal-target="change-state-modal" data-modal-toggle="change-state-modal"
+                  class="mx-2.5 text-blue-700 text-xs px-4 hover:underline">Change State</button>
             </li>
             <li class="text-sm mb-5">
               <PriorityBadge :priority="issue.priority.toUpperCase()"/>
@@ -71,77 +72,6 @@
                   Assign Developer
                 </button>
               </span>
-              <div id="assign-developer-modal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true"
-                   class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                <div class="relative p-4 w-full max-w-md max-h-full">
-                  <!-- Modal content -->
-                  <div class="relative bg-white rounded-lg shadow">
-                    <!-- Modal header -->
-                    <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
-                      <h3 class="text-lg font-semibold text-gray-900">Assign Developer</h3>
-                      <button type="button" data-modal-hide="assign-developer-modal"
-                              class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center">
-                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                             viewBox="0 0 14 14">
-                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                        </svg>
-                        <span class="sr-only">Close modal</span>
-                      </button>
-                    </div>
-                    <!-- Modal body -->
-                    <form class="p-4 md:p-5" v-on:submit.prevent="assignDeveloper">
-                      <div class="grid gap-4 mb-4 grid-cols-2">
-                        <div class="col-span-2">
-                          <h3 class="font-bold text-m ps-0.5">Recommended</h3>
-                          <ul class="max-h-48 overflow-y-auto py-3 px-0.5 text-sm text-gray-700 " aria-labelledby="dropdownRadioButton">
-                            <li v-for="(user, index) in recommendedUserList.slice(0, 3)" :key="index">
-                              <div class="flex items-center p-1 py-2 rounded hover:bg-gray-100">
-                                <input :id="`assignee-${user.username}`" type="radio" :value="user.username" name="default-radio" v-model="assignDeveloperForm.developer"
-                                       class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300">
-                                <label :for="`assignee-${user.username}`"
-                                       class="w-full ms-2 text-sm font-medium text-gray-900 rounded">{{ user.username }}</label>
-                              </div>
-                            </li>
-                          </ul>
-                        </div>
-
-                        <div class="col-span-2">
-                          <div class="relative">
-                            <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
-                              <svg class="w-4 h-4 text-gray-500" aria-hidden="true"
-                                   xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                              </svg>
-                            </div>
-                            <input type="text" id="input-group-search"
-                                   class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded bg-gray-50 focus:border-blue-500"
-                                   placeholder="Search user">
-                          </div>
-                          <ul class="max-h-48 overflow-y-auto py-3 px-0.5 text-sm text-gray-700 " aria-labelledby="dropdownRadioButton">
-                            <li v-for="(user, index) in recommendedUserList" :key="index">
-                              <div class="flex items-center p-1 py-2 rounded hover:bg-gray-100">
-                                <input :id="`assignee-${user.username}`" type="radio" :value="user.username" name="default-radio" v-model="assignDeveloperForm.developer"
-                                       class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300">
-                                <label :for="`assignee-${user.username}`"
-                                       class="w-full ms-2 text-sm font-medium text-gray-900 rounded">{{ user.username }}</label>
-                              </div>
-                            </li>
-                          </ul>
-
-                        </div>
-                      </div>
-                      <button type="submit"
-                              class="text-white inline-flex items-center bg-blue-600 hover:bg-blue-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                              data-modal-hide="assign-developer-modal">
-                        Assign
-                      </button>
-                    </form>
-                  </div>
-                </div>
-              </div>
-
             </li>
             <li class="text-sm mb-5">
               <span class="px-1.5 text-xs font-medium">{{ issue.fixer ? issue.fixer : "Unassigned" }}</span>
@@ -157,7 +87,139 @@
       </div>
     </div>
 
+    <div id="assign-developer-modal" data-modal-backdrop="dynamic" tabindex="-1" aria-hidden="true"
+         class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+      <div class="relative p-4 w-full max-w-md max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow">
+          <!-- Modal header -->
+          <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
+            <h3 class="text-lg font-semibold text-gray-900">Assign Developer</h3>
+            <button type="button" data-modal-hide="assign-developer-modal"
+                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center">
+              <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                   viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+              </svg>
+              <span class="sr-only">Close modal</span>
+            </button>
+          </div>
+          <!-- Modal body -->
+          <form class="p-4 md:p-5" v-on:submit.prevent="assignDeveloper">
+            <div class="grid gap-4 mb-4 grid-cols-2">
+              <div class="col-span-2">
+                <h3 class="font-bold text-m ps-0.5">Recommended</h3>
+                <ul class="max-h-48 overflow-y-auto py-3 px-0.5 text-sm text-gray-700 " aria-labelledby="dropdownRadioButton">
+                  <li v-for="(user, index) in recommendedUserList.slice(0, 3)" :key="index">
+                    <div class="flex items-center p-1 py-2 rounded hover:bg-gray-100">
+                      <input :id="`assignee-${user.username}`" type="radio" :value="user.username" name="default-radio" v-model="assignDeveloperForm.developer"
+                             class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300">
+                      <label :for="`assignee-${user.username}`"
+                             class="w-full ms-2 text-sm font-medium text-gray-900 rounded">{{ user.username }}</label>
+                    </div>
+                  </li>
+                </ul>
+              </div>
 
+              <div class="col-span-2">
+                <div class="relative">
+                  <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg class="w-4 h-4 text-gray-500" aria-hidden="true"
+                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg>
+                  </div>
+                  <input type="text" id="input-group-search"
+                         class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded bg-gray-50 focus:border-blue-500"
+                         placeholder="Search user">
+                </div>
+                <ul class="max-h-48 overflow-y-auto py-3 px-0.5 text-sm text-gray-700 " aria-labelledby="dropdownRadioButton">
+                  <li v-for="(user, index) in recommendedUserList" :key="index">
+                    <div class="flex items-center p-1 py-2 rounded hover:bg-gray-100">
+                      <input :id="`assignee-${user.username}`" type="radio" :value="user.username" name="default-radio" v-model="assignDeveloperForm.developer"
+                             class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300">
+                      <label :for="`assignee-${user.username}`"
+                             class="w-full ms-2 text-sm font-medium text-gray-900 rounded">{{ user.username }}</label>
+                    </div>
+                  </li>
+                </ul>
+
+              </div>
+            </div>
+            <button type="submit" data-popover-target="popover-default" data-popover-placement="right"
+                    class="text-white inline-flex items-center bg-blue-600 hover:bg-blue-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                    data-modal-hide="assign-developer-modal">
+              Assign
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div data-popover id="popover-default" role="tooltip" class="absolute z-50 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800">
+      <div class="px-3 py-2 bg-gray-100 border-b border-gray-200 rounded-t-lg dark:border-gray-600 dark:bg-gray-700">
+        <h3 class="font-semibold text-gray-900 dark:text-white">Status Change</h3>
+      </div>
+      <div class="px-3 py-2">
+        <span>Status of this issue will be changed into 'Assigned'</span>
+      </div>
+    </div>
+
+
+    <div id="change-state-modal" data-modal-backdrop="dynamic" tabindex="-1" aria-hidden="true"
+         class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+      <div class="relative p-4 w-full max-w-md max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow">
+          <!-- Modal header -->
+          <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
+            <h3 class="text-lg font-semibold text-gray-900">Change State</h3>
+            <button type="button" data-modal-hide="change-state-modal"
+                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center">
+              <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                   viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+              </svg>
+              <span class="sr-only">Close modal</span>
+            </button>
+          </div>
+          <!-- Modal body -->
+          <form class="p-4 md:p-5" v-on:submit.prevent="changeState">
+            <div class="grid gap-4 mb-4 grid-cols-2">
+              <div class="col-span-2 flex">
+                <select id="status"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-blue-500 block p-2.5 w-2/5"
+                        disabled>
+                  <option selected>{{changeStateFrom.newState}}</option>
+                </select>
+
+                <svg class="w-1/5 h-6 text-gray-800 my-2.5 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M19 12H5m14 0-4 4m4-4-4-4"/>
+                </svg>
+
+                <select id="priority" v-model="changeStateFrom.newState"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-blue-500 block p-2.5 w-2/5">
+                  <option value="new">New</option>
+                  <option value="assigned">Assigned</option>
+                  <option value="fixed">Fixed</option>
+                  <option value="resolved">Resolved</option>
+                  <option value="closed">Closed</option>
+                  <option value="reopened">Reopened</option>
+                </select>
+              </div>
+            </div>
+            <button type="submit"
+                    class="text-white inline-flex items-center bg-blue-600 hover:bg-blue-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                    data-modal-hide="change-state-modal">
+              Change
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
 
   </section>
   </body>
@@ -192,6 +254,10 @@ const assignDeveloperForm = {
   "developer": ""
 }
 
+const changeStateFrom = {
+  "newState": ""
+}
+
 const recommendedUserList = [
   {
     "username": "hysk",
@@ -221,8 +287,13 @@ const recommendedUserList = [
 ]
 
 onMounted(() => {
-  console.log("monuted")
   initFlowbite();
+  // eslint-disable-next-line
+  // const modal = FlowbiteInstances.getInstance('Modal', 'assign-developer-modal');
+  // window.onpopstate = () => {
+  //   modal.hide()
+  // }
+
   getIssue()
 })
 
@@ -232,6 +303,7 @@ function getIssue() {
       .then(response => {
         console.log(response.data)
         issue.value = response.data
+        changeStateFrom.newState = issue.value.status
       }).catch(error => {
     console.log(error)
     if (error.message.indexOf('Network Error') > -1) {
@@ -262,6 +334,20 @@ function assignDeveloper() {
         console.log(response.data)
         getIssue()
       }).catch(error => {
+    console.log(error)
+    if (error.message.indexOf('Network Error') > -1) {
+      alert('Network Error\nPlease Try again later')
+    }
+  })
+}
+
+function changeState() {
+  axios.put('/api/projects/' + route.params.project_id + '/issues/' + route.params.issue_id + '/status', {
+    "status": changeStateFrom.newState
+  }).then(response => {
+    console.log(response.data)
+    getIssue()
+  }).catch(error => {
     console.log(error)
     if (error.message.indexOf('Network Error') > -1) {
       alert('Network Error\nPlease Try again later')
