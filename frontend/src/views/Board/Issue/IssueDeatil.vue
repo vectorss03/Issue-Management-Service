af058b1d1b7237658912f4c3d5d9b6e05be066fa
hysk <vectorss03@gmail.com>
2024-05-24 15:44:21 +0900

    add Assign Developer Modal

--- frontend/src/views/Board/Issue/IssueDeatil.vue
+++ frontend/src/views/Board/Issue/IssueDeatil.vue
@@ -6,7 +6,7 @@
       <div class="mr-10 w-2/3">
         <div>
           <h1 class="font-bold text-3xl m-3 mb-10">{{ issue.title }}</h1>
-          <h2 class="font-bold text-m m-3">Description</h2>
+          <h2 class="font-semibold text-m m-3">Description</h2>
           <div v-text="issue.description" style="white-space: pre-line; word-break: break-word"
                class="text-sm font-normal text-gray-700 mx-3 mb-10 min-h-40"/>
         </div>
@@ -55,34 +55,109 @@
             <li class="font-semibold text-sm mb-3">Reporter</li>
             <li class="font-semibold text-sm">Reported Date</li>
           </ol>
-
-          <ol class="mx-16 items-center my-3">
-            <li class="mb-1">
+          <ol class="mx-16 items-center my-3 py-1">
+            <li class="text-sm mb-3">
               <StatusBadge :status="issue.status.toUpperCase()"/>
-              <button class="mx-2.5 text-blue-700 text-xs py-2 px-4 hover:underline">Change State</button>
+              <button class="mx-2.5 text-blue-700 text-xs px-4 hover:underline">Change State</button>
             </li>
-            <li class="mb-3">
+            <li class="text-sm mb-5">
               <PriorityBadge :priority="issue.priority.toUpperCase()"/>
             </li>
-            <li class="mb-1">
+            <li class="text-sm mb-3">
               <span class="px-1.5 text-xs font-medium">{{ issue.assignee ? issue.assignee : "Unassigned" }}</span>
-              <button v-if="!issue.assignee" class="mx-4 text-blue-700 text-xs py-2 px-4 hover:underline">Assign
-                Developer
-              </button>
+              <span v-if="!issue.assignee" class="mx-2.5 px-4">
+                <button data-modal-target="assign-developer-modal" data-modal-toggle="assign-developer-modal"
+                        class="text-blue-700 text-xs hover:underline">
+                  Assign Developer
+                </button>
+              </span>
+              <div id="assign-developer-modal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true"
+                   class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                <div class="relative p-4 w-full max-w-md max-h-full">
+                  <!-- Modal content -->
+                  <div class="relative bg-white rounded-lg shadow">
+                    <!-- Modal header -->
+                    <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
+                      <h3 class="text-lg font-semibold text-gray-900">Assign Developer</h3>
+                      <button type="button" data-modal-hide="assign-developer-modal"
+                              class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center">
+                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                             viewBox="0 0 14 14">
+                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                        </svg>
+                        <span class="sr-only">Close modal</span>
+                      </button>
+                    </div>
+                    <!-- Modal body -->
+                    <form class="p-4 md:p-5" v-on:submit.prevent="assignDeveloper">
+                      <div class="grid gap-4 mb-4 grid-cols-2">
+                        <div class="col-span-2">
+                          <h3 class="font-bold text-m ps-0.5">Recommended</h3>
+                          <ul class="max-h-48 overflow-y-auto py-3 px-0.5 text-sm text-gray-700 " aria-labelledby="dropdownRadioButton">
+                            <li v-for="(user, index) in recommendedUserList.slice(0, 3)" :key="index">
+                              <div class="flex items-center p-1 py-2 rounded hover:bg-gray-100">
+                                <input :id="`assignee-${user.username}`" type="radio" :value="user.username" name="default-radio" v-model="assignDeveloperForm.developer"
+                                       class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300">
+                                <label :for="`assignee-${user.username}`"
+                                       class="w-full ms-2 text-sm font-medium text-gray-900 rounded">{{ user.username }}</label>
+                              </div>
+                            </li>
+                          </ul>
+                        </div>
+
+                        <div class="col-span-2">
+                          <div class="relative">
+                            <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+                              <svg class="w-4 h-4 text-gray-500" aria-hidden="true"
+                                   xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                              </svg>
+                            </div>
+                            <input type="text" id="input-group-search"
+                                   class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded bg-gray-50 focus:border-blue-500"
+                                   placeholder="Search user">
+                          </div>
+                          <ul class="max-h-48 overflow-y-auto py-3 px-0.5 text-sm text-gray-700 " aria-labelledby="dropdownRadioButton">
+                            <li v-for="(user, index) in recommendedUserList" :key="index">
+                              <div class="flex items-center p-1 py-2 rounded hover:bg-gray-100">
+                                <input :id="`assignee-${user.username}`" type="radio" :value="user.username" name="default-radio" v-model="assignDeveloperForm.developer"
+                                       class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300">
+                                <label :for="`assignee-${user.username}`"
+                                       class="w-full ms-2 text-sm font-medium text-gray-900 rounded">{{ user.username }}</label>
+                              </div>
+                            </li>
+                          </ul>
+
+                        </div>
+                      </div>
+                      <button type="submit"
+                              class="text-white inline-flex items-center bg-blue-600 hover:bg-blue-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                              data-modal-hide="assign-developer-modal">
+                        Assign
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+
             </li>
-            <li class="mb-3.5">
+            <li class="text-sm mb-5">
               <span class="px-1.5 text-xs font-medium">{{ issue.fixer ? issue.fixer : "Unassigned" }}</span>
             </li>
-            <li class="mb-2">
+            <li class="text-sm mb-3">
               <span class="px-1.5 text-xs font-medium">{{ issue.reporter }}</span>
             </li>
-            <li class="">
+            <li class="text-sm">
               <span class="px-1.5 text-xs font-medium">{{ issue.reported_date }}</span>
             </li>
           </ol>
         </div>
       </div>
     </div>
+
+
 
   </section>
   </body>
@@ -93,6 +168,7 @@ import {inject, onMounted, ref} from "vue";
 import {useRoute} from "vue-router";
 import StatusBadge from "@/components/StatusBadge.vue";
 import PriorityBadge from "@/components/PriorityBadge.vue";
+import {initFlowbite} from "flowbite";
 
 const axios = inject('axios')
 const route = useRoute()
@@ -112,23 +188,56 @@ const commentForm = {
   "content": ""
 }
 
+const assignDeveloperForm = {
+  "developer": ""
+}
+
+const recommendedUserList = [
+  {
+    "username": "hysk",
+  },
+  {
+    "username": "khw",
+  },
+  {
+    "username": "kss",
+  },
+  {
+    "username": "kss",
+  },
+  {
+    "username": "kss",
+  },
+  {
+    "username": "kss",
+  },
+  {
+    "username": "kss",
+  },
+  {
+    "username": "kss",
+  },
+
+]
+
 onMounted(() => {
   console.log("monuted")
+  initFlowbite();
   getIssue()
 })
 
 function getIssue() {
   console.log("getIssue")
   axios.get('/api/projects/' + route.params.project_id + '/issues/' + route.params.issue_id)
-    .then(response => {
-      console.log(response.data)
-      issue.value = response.data
-    }).catch(error => {
-      console.log(error)
-      if (error.message.indexOf('Network Error') > -1) {
-        alert('Network Error\nPlease Try again later')
-      }
-    })
+      .then(response => {
+        console.log(response.data)
+        issue.value = response.data
+      }).catch(error => {
+    console.log(error)
+    if (error.message.indexOf('Network Error') > -1) {
+      alert('Network Error\nPlease Try again later')
+    }
+  })
 }
 
 function postComment() {
@@ -139,6 +248,20 @@ function postComment() {
     getIssue()
     commentForm.content = ""
   }).catch(error => {
+    console.log(error)
+    if (error.message.indexOf('Network Error') > -1) {
+      alert('Network Error\nPlease Try again later')
+    }
+  })
+}
+
+function assignDeveloper() {
+  axios.put('/api/projects/' + route.params.project_id + '/issues/' + route.params.issue_id + '/assignee', {
+    "assignee": assignDeveloperForm.developer
+  }).then(response => {
+        console.log(response.data)
+        getIssue()
+      }).catch(error => {
     console.log(error)
     if (error.message.indexOf('Network Error') > -1) {
       alert('Network Error\nPlease Try again later')
